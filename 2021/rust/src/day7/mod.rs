e3e8b031756f656da78efee6aedc6cf3dfcfe79b
Muhammad Falak R Wani <falakreyaz@gmail.com>
2021-12-07 11:39:24 +0530

    2021: rust: day7: add soln to part2
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- 2021/rust/src/day7/mod.rs
+++ 2021/rust/src/day7/mod.rs
@@ -4,6 +4,7 @@ pub fn main() -> Result<(), std::io::Error> {
     let input = get_input()?;
 
     println!("Day7/Part1 Sol: {}", part1(&input));
+    println!("Day7/Part2 Sol: {}", part2(&input));
     Ok(())
 }
 
@@ -13,6 +14,22 @@ fn part1(input: &[i32]) -> usize {
     (min..=max)
         .into_iter()
         .map(|i| input.iter().map(|j| ((j - i).abs()) as usize).sum())
+        .min()
+        .unwrap()
+}
+
+fn part2(input: &[i32]) -> usize {
+    let min = *input.into_iter().min().unwrap();
+    let max = *input.into_iter().max().unwrap();
+    (min..=max)
+        .into_iter()
+        .map(|i| {
+            input
+                .iter()
+                .map(|j| ((j - i).abs()) as usize)
+                .map(|n| n * (n + 1) / 2)
+                .sum()
+        })
         .min()
         .unwrap()
 }
@@ -41,5 +58,11 @@ mod tests {
         let str_in = "16,1,2,0,4,2,7,1,2,14";
         let raw_input = parse_input(str_in).unwrap();
         assert_eq!(37, part1(&raw_input));
+    }
+    #[test]
+    fn part2_test() {
+        let str_in = "16,1,2,0,4,2,7,1,2,14";
+        let raw_input = parse_input(str_in).unwrap();
+        assert_eq!(168, part2(&raw_input));
     }
 }
