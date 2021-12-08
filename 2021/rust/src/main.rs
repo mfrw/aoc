971963345656c043c0ba27b775b47610cba1aa78
Muhammad Falak R Wani <falakreyaz@gmail.com>
2021-12-08 15:07:27 +0530

    2021: rust: day8: add part1 solution
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- 2021/rust/src/main.rs
+++ 2021/rust/src/main.rs
@@ -7,6 +7,7 @@ mod day4;
 mod day5;
 mod day6;
 mod day7;
+mod day8;
 
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     day1::main()?;
@@ -16,5 +17,6 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     day5::main()?;
     day6::main()?;
     day7::main()?;
+    day8::main()?;
     Ok(())
 }
