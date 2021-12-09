5bdd084e37af8b01abc929aa20edef1b4cfeeec6
Muhammad Falak R Wani <falakreyaz@gmail.com>
2021-12-09 14:14:40 +0530

    2021: rust: day9: add solution for part2
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- 2021/rust/src/day9/mod.rs
+++ 2021/rust/src/day9/mod.rs
@@ -3,7 +3,43 @@ use crate::utils;
 pub fn main() -> Result<(), std::io::Error> {
     let input = get_input()?;
     println!("Day9/Part1 Sol: {}", part1(&input));
+    println!("Day9/Part2 Sol: {}", part2(&input));
     Ok(())
+}
+
+fn part2(input: &[Vec<u8>]) -> usize {
+    use std::collections::HashSet;
+    fn dfs(i: isize, j: isize, grid: &[Vec<u8>], visited: &mut HashSet<(isize, isize)>) {
+        if i < 0
+            || j < 0
+            || i >= grid.len() as isize
+            || j >= grid[i as usize].len() as isize
+            || visited.contains(&(i, j))
+            || grid[i as usize][j as usize] == 9
+        {
+            return;
+        }
+        visited.insert((i, j));
+        dfs(i - 1, j, grid, visited);
+        dfs(i + 1, j, grid, visited);
+        dfs(i, j - 1, grid, visited);
+        dfs(i, j + 1, grid, visited);
+    }
+
+    let mut visited = HashSet::new();
+
+    let mut memo = vec![];
+    for (i, row) in input.iter().enumerate() {
+        for j in 0..row.len() {
+            if (!visited.contains(&(i as isize, j as isize))) && input[i][j] != 9 {
+                let prev = visited.len();
+                dfs(i as isize, j as isize, &input, &mut visited);
+                memo.push(visited.len() - prev);
+            }
+        }
+    }
+    memo.sort();
+    memo.into_iter().rev().take(3).fold(1, |acc, p| acc * p)
 }
 
 fn part1(input: &[Vec<u8>]) -> usize {
@@ -53,9 +89,25 @@ mod tests {
     use super::*;
 
     #[test]
-    fn part1_test() {
+    fn part1_custom_test() {
         let i = "2199943210\n 3987894921\n 9856789892\n 8767896789\n 9899965678\n";
         let input = parse_input(&i).unwrap();
         assert_eq!(15, part1(&input));
+    }
+    #[test]
+    fn part2_custom_test() {
+        let i = "2199943210\n 3987894921\n 9856789892\n 8767896789\n 9899965678\n";
+        let input = parse_input(&i).unwrap();
+        assert_eq!(1134, part2(&input));
+    }
+    #[test]
+    fn part1_test() {
+        let input = get_input().unwrap();
+        assert_eq!(572, part1(&input));
+    }
+    #[test]
+    fn part2_test() {
+        let input = get_input().unwrap();
+        assert_eq!(847044, part2(&input));
     }
 }
