ce4c3d7e5e54bddac01c7b1cbd659d8c934a0e1b
Muhammad Falak R Wani <falakreyaz@gmail.com>
2021-12-19 17:48:37 +0530

    2021: rust: day19: add solution to part1
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- 2021/rust/src/main.rs
+++ 2021/rust/src/main.rs
@@ -10,6 +10,7 @@ mod day15;
 mod day16;
 mod day17;
 mod day18;
+mod day19;
 mod day2;
 mod day3;
 mod day4;
@@ -38,5 +39,6 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     day16::main()?;
     day17::main()?;
     day18::main()?;
+    day19::main()?;
     Ok(())
 }
