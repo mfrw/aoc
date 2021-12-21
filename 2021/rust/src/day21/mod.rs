6e22b4917fbf64ec78e156994b39cc582cc7cf54
Muhammad Falak R Wani <falakreyaz@gmail.com>
2021-12-21 12:21:30 +0530

    2021: rust: day21: add part2 solution
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- 2021/rust/src/day21/mod.rs
+++ 2021/rust/src/day21/mod.rs
@@ -1,6 +1,14 @@
+use itertools::Itertools;
+use std::collections::HashMap;
+
 pub fn main() -> Result<(), std::io::Error> {
     println!("Day21/Part1 Sol: {}", part1(10, 3));
+    println!("Day21/Part2 Sol: {}", part2(10, 3));
     Ok(())
+}
+
+fn part2(pos1: usize, pos2: usize) -> usize {
+    play_game(&mut HashMap::new(), 0, 0, pos1, pos2, true).0
 }
 
 fn part1(pos1: usize, pos2: usize) -> usize {
@@ -42,3 +50,42 @@ fn part1(pos1: usize, pos2: usize) -> usize {
     nrolls * if p1 >= 1000 { p2 } else { p1 }
 }
 
+type Cache = HashMap<(usize, usize, usize, usize, bool), (usize, usize)>;
+
+fn play_game(
+    cache: &mut Cache,
+    p1: usize,
+    p2: usize,
+    pos1: usize,
+    pos2: usize,
+    turn1: bool,
+) -> (usize, usize) {
+    if p1 >= 21 {
+        return (1, 0);
+    }
+    if p2 >= 21 {
+        return (0, 1);
+    }
+    if let Some(&score) = cache.get(&(p1, p2, pos1, pos2, turn1)) {
+        return score;
+    }
+    let mut score = (0, 0);
+    for ((d1, d2), d3) in [1, 2, 3]
+        .iter()
+        .cartesian_product([1, 2, 3])
+        .cartesian_product([1, 2, 3])
+    {
+        let die = d1 + d2 + d3;
+        let (s1, s2) = if turn1 {
+            let pos1 = pos1 + die - if pos1 + die > 10 { 10 } else { 0 };
+            play_game(cache, p1 + pos1, p2, pos1, pos2, false)
+        } else {
+            let pos2 = pos2 + die - if pos2 + die > 10 { 10 } else { 0 };
+            play_game(cache, p1, p2 + pos2, pos1, pos2, true)
+        };
+        score.0 += s1;
+        score.1 += s2;
+    }
+    cache.insert((p1, p2, pos1, pos2, turn1), score);
+    score
+}
