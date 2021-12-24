9d91e96a814de6d7fc9c24c8b23cfce8b866c30e
Muhammad Falak R Wani <falakreyaz@gmail.com>
2021-12-24 16:32:50 +0530

    2021: rust: day24: refactor to more generic code
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- 2021/rust/src/day24/mod.rs
+++ 2021/rust/src/day24/mod.rs
@@ -1,171 +1,278 @@
-use crate::utils;
+use std::str::FromStr;
 
-use std::collections::HashMap;
+use crate::utils;
 
 pub fn main() -> Result<(), std::io::Error> {
     let program = get_input()?;
-    println!("Day24/Part1 Sol: {}", part1(&program));
-    //println!("Day24/Part2 Sol: {}", part2(&program));
+    println!("Day24/Part1 Sol: {}", part1(&program).unwrap());
+    println!("Day24/Part2 Sol: {}", part2(&program).unwrap());
     Ok(())
 }
 
-fn part1(program: &[Instruction]) -> i64 {
-    solve(program, false)
-}
-
-#[allow(dead_code)]
-fn part2(program: &[Instruction]) -> i64 {
-    solve(program, true)
-}
-
-enum RegOrVal {
-    Reg(usize),
-    Val(i64),
-}
-
-impl RegOrVal {
-    fn to_val(&self, reg: &[i64; 4]) -> i64 {
-        match self {
-            RegOrVal::Reg(i) => reg[*i],
-            RegOrVal::Val(v) => *v,
-        }
-    }
-}
-
-enum Instruction {
-    Inp(usize),
-    Add(usize, RegOrVal),
-    Mul(usize, RegOrVal),
-    Div(usize, RegOrVal),
-    Mod(usize, RegOrVal),
-    Eql(usize, RegOrVal),
-}
-
-impl Instruction {
-    fn exec(&self, reg: &mut [i64; 4], input: Option<i64>) {
-        match self {
-            Instruction::Inp(a) => {
-                reg[*a] = input.unwrap();
-            }
-            Instruction::Add(a, b) => {
-                reg[*a] += b.to_val(reg);
-            }
-            Instruction::Mul(a, b) => {
-                reg[*a] *= b.to_val(reg);
-            }
-            Instruction::Div(a, b) => {
-                reg[*a] /= b.to_val(reg);
-            }
-            Instruction::Mod(a, b) => {
-                reg[*a] %= b.to_val(reg);
-            }
-            Instruction::Eql(a, b) => {
-                reg[*a] = if reg[*a] == b.to_val(reg) { 1 } else { 0 };
-            }
-        }
-    }
-}
-
-fn reg_idx(reg: char) -> usize {
-    match reg {
-        'w' => 0,
-        'x' => 1,
-        'y' => 2,
-        'z' => 3,
-        _ => panic!(),
-    }
-}
-
-fn best(
-    program: &[Instruction],
-    pc: usize,
-    reg: [i64; 4],
-    visited: &mut HashMap<([i64; 4], usize), Option<i64>>,
-    smallest: bool,
-) -> Option<i64> {
-    assert!(matches!(program[pc], Instruction::Inp(_)));
-
-    if let Some(answer) = visited.get(&(reg, pc)) {
-        return *answer;
-    }
-
-    let range = if smallest {
-        [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    } else {
-        [9, 8, 7, 6, 5, 4, 3, 2, 1]
-    };
-    'inputs: for input in range {
-        let mut reg = reg;
-        let mut pc = pc;
-        program[pc].exec(&mut reg, Some(input));
-        pc += 1;
-
-        while let Some(inst) = program.get(pc) {
-            if matches!(program[pc], Instruction::Inp(_)) {
-                if let Some(best) = best(program, pc, reg, visited, smallest) {
-                    visited.insert((reg, pc), Some(best * 10 + input));
-                    return Some(best * 10 + input);
-                } else {
-                    continue 'inputs;
-                }
-            } else {
-                inst.exec(&mut reg, None);
-                pc += 1;
-            }
-        }
-
-        if reg[3] == 0 {
-            visited.insert((reg, pc), Some(input));
-            return Some(input);
-        }
-    }
-
-    visited.insert((reg, pc), None);
-    None
-}
-
-fn solve(program: &[Instruction], smallest: bool) -> i64 {
-    let mut memo = HashMap::new();
-    let answer = best(program, 0, [0; 4], &mut memo, smallest);
-    format!("{}", answer.unwrap())
+fn convert_to_vec_i32(n: i64) -> Option<Vec<i32>> {
+    let s = n.to_string();
+    let v = s
         .chars()
-        .rev()
-        .collect::<String>()
-        .parse()
-        .unwrap()
+        .map(|ch| ch.to_string().parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+    if v.contains(&0) {
+        None
+    } else {
+        Some(v)
+    }
 }
 
-fn get_input() -> Result<Vec<Instruction>, std::io::Error> {
+fn part2(program: &[Ins]) -> Option<i64> {
+    let mut alu = ALU::new();
+    let i = 48111514719111;
+    let input = convert_to_vec_i32(i).unwrap();
+    alu.run(program, &input).unwrap();
+    if alu.z() == &0 {
+        Some(i)
+    } else {
+        None
+    }
+}
+
+fn part1(program: &[Ins]) -> Option<i64> {
+    let mut alu = ALU::new();
+    let i = 99995969919326;
+    let input = convert_to_vec_i32(i).unwrap();
+    alu.run(program, &input).unwrap();
+    if alu.z() == &0 {
+        Some(i)
+    } else {
+        None
+    }
+}
+
+fn get_input() -> Result<Vec<Ins>, std::io::Error> {
     let input = utils::get_input("input/day24")?;
     parse_input(&input)
 }
 
-fn parse_input(input: &str) -> Result<Vec<Instruction>, std::io::Error> {
-    let mut program = Vec::new();
+fn parse_input(input: &str) -> Result<Vec<Ins>, std::io::Error> {
+    let v = input
+        .lines()
+        .map(|l| Ins::from_str(l).unwrap())
+        .collect::<Vec<_>>();
+    Ok(v)
+}
 
-    for line in input.trim().split('\n') {
-        let mut parts = line.split(' ');
-        let instruction = parts.next().unwrap();
-        let reg: char = parts.next().unwrap().parse().unwrap();
-        let b_value = parts.next().map(|a| {
-            a.parse::<i64>()
-                .map(RegOrVal::Val)
-                .unwrap_or_else(|_| RegOrVal::Reg(reg_idx(a.parse::<char>().unwrap())))
-        });
+enum Register {
+    W,
+    X,
+    Y,
+    Z,
+}
 
-        let reg = reg_idx(reg);
+impl FromStr for Register {
+    type Err = std::num::ParseIntError;
 
-        program.push(match (instruction, reg, b_value) {
-            ("inp", reg, None) => Instruction::Inp(reg),
-            ("add", a, Some(b)) => Instruction::Add(a, b),
-            ("mul", a, Some(b)) => Instruction::Mul(a, b),
-            ("div", a, Some(b)) => Instruction::Div(a, b),
-            ("mod", a, Some(b)) => Instruction::Mod(a, b),
-            ("eql", a, Some(b)) => Instruction::Eql(a, b),
-            _ => {
-                panic!();
-            }
-        });
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let reg = match s {
+            "x" => Register::X,
+            "y" => Register::Y,
+            "w" => Register::W,
+            "z" => Register::Z,
+            _ => panic!("Got {}", s),
+        };
+        Ok(reg)
     }
-    Ok(program)
+}
+
+enum Operand {
+    Var(Register),
+    Imm(i32),
+}
+
+impl FromStr for Operand {
+    type Err = std::num::ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let op = match s {
+            "x" => Operand::Var(Register::X),
+            "w" => Operand::Var(Register::W),
+            "y" => Operand::Var(Register::Y),
+            "z" => Operand::Var(Register::Z),
+            _ => {
+                let im = s.parse::<i32>()?;
+                Operand::Imm(im)
+            }
+        };
+        Ok(op)
+    }
+}
+
+enum Ins {
+    Inp(Register),
+    Add(Register, Operand),
+    Mul(Register, Operand),
+    Div(Register, Operand),
+    Mod(Register, Operand),
+    Eql(Register, Operand),
+}
+
+impl FromStr for Ins {
+    type Err = std::num::ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = s.split(" ").collect::<Vec<_>>();
+        let ins = match parts[0] {
+            "inp" => {
+                let reg = Register::from_str(parts[1])?;
+                Ins::Inp(reg)
+            }
+            "add" => {
+                let op1 = Register::from_str(parts[1])?;
+                let op2 = Operand::from_str(parts[2])?;
+                Ins::Add(op1, op2)
+            }
+            "mul" => {
+                let op1 = Register::from_str(parts[1])?;
+                let op2 = Operand::from_str(parts[2])?;
+                Ins::Mul(op1, op2)
+            }
+            "div" => {
+                let op1 = Register::from_str(parts[1])?;
+                let op2 = Operand::from_str(parts[2])?;
+                Ins::Div(op1, op2)
+            }
+            "mod" => {
+                let op1 = Register::from_str(parts[1])?;
+                let op2 = Operand::from_str(parts[2])?;
+                Ins::Mod(op1, op2)
+            }
+            "eql" => {
+                let op1 = Register::from_str(parts[1])?;
+                let op2 = Operand::from_str(parts[2])?;
+                Ins::Eql(op1, op2)
+            }
+            _ => panic!("Got {}", parts[0]),
+        };
+        Ok(ins)
+    }
+}
+
+struct ALU {
+    w: i32,
+    x: i32,
+    y: i32,
+    z: i32,
+    pos: usize,
+}
+
+impl ALU {
+    fn new() -> Self {
+        ALU {
+            w: 0,
+            x: 0,
+            y: 0,
+            z: 0,
+            pos: 0,
+        }
+    }
+
+    fn run(&mut self, instructions: &[Ins], input: &[i32]) -> Result<(), std::io::Error> {
+        if self.pos == input.len() {
+            return Ok(());
+        }
+
+        for ins in instructions {
+            match ins {
+                Ins::Inp(r) => {
+                    let val = input[self.pos];
+                    self.pos += 1;
+                    self.input(r, &val)?;
+                }
+                Ins::Add(a, b) => {
+                    self.binop(a, b, "+")?;
+                }
+                Ins::Mul(a, b) => {
+                    self.binop(a, b, "*")?;
+                }
+                Ins::Div(a, b) => {
+                    self.binop(a, b, "/")?;
+                }
+                Ins::Mod(a, b) => {
+                    self.binop(a, b, "%")?;
+                }
+                Ins::Eql(a, b) => {
+                    self.binop(a, b, "==")?;
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn binop(&mut self, a: &Register, b: &Operand, op: &str) -> Result<(), std::io::Error> {
+        let op2 = match b {
+            Operand::Imm(a) => *a,
+            Operand::Var(r) => match r {
+                Register::W => self.w,
+                Register::X => self.x,
+                Register::Y => self.y,
+                Register::Z => self.z,
+            },
+        };
+
+        let input = match a {
+            Register::W => &mut self.w,
+            Register::X => &mut self.x,
+            Register::Y => &mut self.y,
+            Register::Z => &mut self.z,
+        };
+
+        match op {
+            "+" => {
+                *input = *input + op2;
+                Ok(())
+            }
+            "-" => {
+                *input = *input - op2;
+                Ok(())
+            }
+            "*" => {
+                *input = *input * op2;
+                Ok(())
+            }
+            "%" => {
+                *input = *input % op2;
+                Ok(())
+            }
+            "==" => {
+                if *input == op2 {
+                    *input = 1;
+                } else {
+                    *input = 0;
+                }
+                Ok(())
+            }
+            "/" => {
+                if op2 == 0 {
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::InvalidInput,
+                        "Op2==0",
+                    ));
+                }
+                *input = *input / op2;
+                Ok(())
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    fn input(&mut self, reg: &Register, val: &i32) -> Result<(), std::io::Error> {
+        match reg {
+            Register::W => self.w = *val,
+            Register::X => self.x = *val,
+            Register::Y => self.y = *val,
+            Register::Z => self.z = *val,
+        }
+        Ok(())
+    }
+
+    fn z(&self) -> &i32 {
+        &self.z
+    }
 }
