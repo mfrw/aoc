c67a5634ec9a97c7d4d43e7d6dcbd0373a85a70b
Muhammad Falak R Wani <falakreyaz@gmail.com>
2022-01-02 20:40:33 +0530

    2021: rust: utils: stack: improve & learn
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- 2021/rust/src/utils/stack.rs
+++ 2021/rust/src/utils/stack.rs
@@ -3,6 +3,14 @@ pub struct Stack<T> {
     base: Vec<T>,
 }
 
+impl<T> FromIterator<T> for Stack<T> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        Self {
+            base: iter.into_iter().collect::<Vec<_>>(),
+        }
+    }
+}
+
 impl<T> Stack<T> {
     pub fn new() -> Self {
         Self {
@@ -32,10 +40,68 @@ impl<T> Stack<T> {
     }
 }
 
-impl<T> Iterator for Stack<T> {
+impl<T> Stack<T> {
+    #[allow(dead_code)]
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            next: self.base.iter(),
+        }
+    }
+}
+
+pub struct Iter<'a, T: 'a> {
+    next: std::slice::Iter<'a, T>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.next_back()
+    }
+}
+
+pub struct IntoIter<T> {
+    it: Stack<T>,
+}
+
+impl<T> Iterator for IntoIter<T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.base.pop()
+        self.it.pop()
+    }
+}
+
+impl<T> IntoIterator for Stack<T> {
+    type Item = T;
+
+    type IntoIter = IntoIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        IntoIter { it: self }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn stack_test() {
+        let stk = Stack {
+            base: vec![1, 2, 3, 4, 5],
+        };
+        let mut tmp = 0;
+        for &i in stk.iter() {
+            tmp += i;
+        }
+        assert_eq!(tmp, 15);
+    }
+
+    #[test]
+    fn from_slice_test() {
+        let v = vec![1, 2, 3, 4];
+        let stk = v.into_iter().collect::<Stack<_>>();
+        assert_eq!(stk.len(), 4);
     }
 }
