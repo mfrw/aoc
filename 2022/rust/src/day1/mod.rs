4534d20e80e00945ad8121ca7eac74e5b3714e40
Muhammad Falak R Wani <falakreyaz@gmail.com>
2022-12-01 13:28:08 +0530

    2022: rust: day1: add part 2 solution
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- 2022/rust/src/day1/mod.rs
+++ 2022/rust/src/day1/mod.rs
@@ -4,7 +4,9 @@ use std::error::Error;
 pub fn main() -> Result<(), Box<dyn Error>> {
     let input = utils::get_input("input/day1")?;
     let sol1 = part1(input.clone())?;
+    let sol2 = part2(input)?;
     println!("Day1/Part1 Sol: {}", sol1);
+    println!("Day1/Part2 Sol: {}", sol2);
     Ok(())
 }
 
@@ -17,4 +19,13 @@ fn part1(input: String) -> Result<i32, Box<dyn Error>> {
         }
     }
     Ok(mx)
+}
+
+fn part2(input: String) -> Result<i32, Box<dyn Error>> {
+    let mut v = input
+        .split("\n\n")
+        .map(|elf| elf.lines().map(|l| l.parse::<i32>().unwrap()).sum())
+        .collect::<Vec<i32>>();
+    v.sort();
+    Ok(v.into_iter().rev().take(3).sum())
 }
