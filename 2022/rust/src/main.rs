943e431a3dd6282ad8ebb5f24a47ccae08161df6
Muhammad Falak R Wani <falakreyaz@gmail.com>
2022-12-02 13:14:58 +0530

    2022: rust: day2: refactor to reduce boiler plate
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- 2022/rust/src/main.rs
+++ 2022/rust/src/main.rs
@@ -1,12 +1,13 @@
 #![feature(binary_heap_into_iter_sorted)]
 
 mod utils;
+use crate::utils::Solver;
 
 mod day1;
 mod day2;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    day1::main()?;
-    day2::main()?;
+    day1::Solver.solve()?;
+    day2::Solver.solve()?;
     Ok(())
 }
