eb889d29340704d196b0d1151dcab8215bad31d1
Muhammad Falak R Wani <falakreyaz@gmail.com>
2022-12-04 14:14:46 +0530

    2022: rust: day4: add solution
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- 2022/rust/src/main.rs
+++ 2022/rust/src/main.rs
@@ -6,10 +6,12 @@ use crate::utils::Solver;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     day1::Solver.solve()?;
     day2::Solver.solve()?;
     day3::Solver.solve()?;
+    day4::Solver.solve()?;
     Ok(())
 }
