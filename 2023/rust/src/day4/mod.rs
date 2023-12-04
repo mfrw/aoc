d201d0ed9870d129e058e0f3fd2a7be4df2c9f64
Muhammad Falak R Wani <falakreyaz@gmail.com>
2023-12-04 12:39:55 +0530

    2023: rust: day4: add part1
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

---
+++ 2023/rust/src/day4/mod.rs
@@ -0,0 +1,91 @@
+use crate::utils;
+use std::collections::HashSet;
+
+use nom::{
+    bytes::complete::{tag, take_while1},
+    character::complete::multispace1,
+    combinator::map_res,
+    multi::separated_list0,
+    sequence::{delimited, preceded, terminated, tuple},
+    IResult,
+};
+
+pub struct Solver;
+
+impl utils::Solver<4> for Solver {
+    type Part1 = u32;
+    type Part2 = u32;
+
+    fn part1(&self, input: &str) -> Result<Self::Part1, Box<dyn std::error::Error>> {
+        Ok(part1_int(input).unwrap())
+    }
+
+    fn part2(&self, input: &str) -> Result<Self::Part2, Box<dyn std::error::Error>> {
+        Ok(part2_int(input).unwrap())
+    }
+}
+
+fn part1_int(input: &str) -> Option<u32> {
+    let mut ans = 0;
+    for line in input.lines() {
+        match parse_card(line) {
+            Ok((_, c)) => {
+                //println!("{:?} - {:?} - {:?} ", c.id, c.got, c.win);
+                let st1 = HashSet::<usize>::from_iter(c.got);
+                let st2 = HashSet::<usize>::from_iter(c.win);
+                let cnt = st1.intersection(&st2).count();
+                if cnt == 0 {
+                    continue;
+                }
+                ans += 2u32.pow((cnt - 1) as u32);
+            }
+            Err(e) => println!("{e}"),
+        }
+    }
+    Some(ans)
+}
+
+fn part2_int(input: &str) -> Option<u32> {
+    todo!()
+}
+
+#[derive(Debug)]
+struct Card {
+    id: usize,
+    got: Vec<usize>,
+    win: Vec<usize>,
+}
+
+fn parse_card(i: &str) -> IResult<&str, Card> {
+    let (i, (_, id, got, win)) = tuple((
+        terminated(tag("Card"), multispace1),
+        terminated(parse_number, terminated(tag(":"), multispace1)),
+        parse_number_list,
+        preceded(
+            delimited(multispace1, tag("|"), multispace1),
+            parse_number_list,
+        ),
+    ))(i)?;
+    Ok((i, Card { id, got, win }))
+}
+
+fn parse_number_list(i: &str) -> IResult<&str, Vec<usize>> {
+    separated_list0(multispace1, parse_number)(i)
+}
+
+fn parse_number(i: &str) -> IResult<&str, usize> {
+    map_res(take_while1(|c: char| c.is_ascii_digit()), |s: &str| {
+        s.parse::<usize>()
+    })(i)
+}
+
+#[test]
+fn p1_test() {
+    let i = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+    assert_eq!(Some(13), part1_int(i))
+}
