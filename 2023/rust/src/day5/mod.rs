643b4e416c8263911cbbe2555909048e252b3723
Muhammad Falak R Wani <falakreyaz@gmail.com>
2023-12-05 13:39:26 +0530

    2023: rust: day5: brute force part2
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- 2023/rust/src/day5/mod.rs
+++ 2023/rust/src/day5/mod.rs
@@ -51,7 +51,30 @@ fn part1_int(input: &str) -> Option<i64> {
 }
 
 fn part2_int(input: &str) -> Option<i64> {
-    Some(2)
+    let mut parts = input.split("\n\n");
+    let seeds: Vec<(i64, i64)> = parts
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .skip(1)
+        .map(|x| x.parse().unwrap())
+        .collect::<Vec<_>>()
+        .chunks(2)
+        .map(|s| (s[0], s[1]))
+        .collect();
+    let maps: Vec<_> = parts.map(|pt| parse_map(pt)).collect();
+    seeds
+        .into_iter()
+        .flat_map(|(a, b)| {
+            (a..a + b).map(|seed| {
+                maps.iter().fold(seed, |seed, map| {
+                    map.iter()
+                        .find_map(|(range, offset)| range.contains(&seed).then_some(seed + offset))
+                        .unwrap_or(seed)
+                })
+            })
+        })
+        .min()
 }
 
 #[test]
@@ -90,4 +113,42 @@ humidity-to-location map:
 60 56 37
 56 93 4";
     assert_eq!(part1_int(i), Some(35))
+}
+
+#[test]
+fn p2_test() {
+    let i = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+    assert_eq!(part2_int(i), Some(46))
 }
