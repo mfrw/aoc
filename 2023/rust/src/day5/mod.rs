d02969c2e04aa77ca1da0cd0f4de0c7f480c91bb
Muhammad Falak R Wani <falakreyaz@gmail.com>
2023-12-05 11:39:46 +0530

    2023: rust: day5: add part1
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

---
+++ 2023/rust/src/day5/mod.rs
@@ -0,0 +1,93 @@
+use crate::utils;
+use std::ops::Range;
+
+pub struct Solver;
+
+impl utils::Solver<5> for Solver {
+    type Part1 = i64;
+    type Part2 = i64;
+
+    fn part1(&self, input: &str) -> Result<Self::Part1, Box<dyn std::error::Error>> {
+        Ok(part1_int(input).unwrap())
+    }
+
+    fn part2(&self, input: &str) -> Result<Self::Part2, Box<dyn std::error::Error>> {
+        Ok(part2_int(input).unwrap())
+    }
+}
+
+fn parse_map(input: &str) -> Vec<(Range<i64>, i64)> {
+    let mut result = Vec::new();
+    for line in input.lines().skip(1) {
+        let ns: Vec<i64> = line
+            .split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect();
+        result.push((ns[1]..ns[1] + ns[2], ns[0] - ns[1]))
+    }
+    result
+}
+
+fn part1_int(input: &str) -> Option<i64> {
+    let mut parts = input.split("\n\n");
+    let seeds: Vec<i64> = parts
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .skip(1)
+        .map(|x| x.parse().unwrap())
+        .collect();
+    let maps: Vec<_> = parts.map(|pt| parse_map(pt)).collect();
+    seeds
+        .into_iter()
+        .map(|seed| {
+            maps.iter().fold(seed, |seed, map| {
+                map.iter()
+                    .find_map(|(range, offset)| range.contains(&seed).then_some(seed + offset))
+                    .unwrap_or(seed)
+            })
+        })
+        .min()
+}
+
+fn part2_int(input: &str) -> Option<i64> {
+    Some(2)
+}
+
+#[test]
+fn p1_test() {
+    let i = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+    assert_eq!(part1_int(i), Some(35))
+}
--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -6,11 +6,13 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     day1::Solver.solve()?;
     day2::Solver.solve()?;
     day3::Solver.solve()?;
     day4::Solver.solve()?;
+    day5::Solver.solve()?;
     Ok(())
 }
