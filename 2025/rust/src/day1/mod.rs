1a24aa34b430c5dcd4d34175774c4adc4aaab293
Muhammad Falak R Wani <falakreyaz@gmail.com>
2025-12-01 15:22:11 +0530

    2025: rust: day1: impl part2
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- 2025/rust/src/day1/mod.rs
+++ 2025/rust/src/day1/mod.rs
@@ -29,6 +29,30 @@ impl utils::Solver<1> for Solver {
     }
 
     fn part2(&self, input: &str) -> Result<Self::Part2, Box<dyn std::error::Error>> {
-        todo!()
+        let mut count = 0;
+        let mut dial: [u32; 100] = std::array::from_fn(|i| i as u32);
+        for line in input.lines() {
+            let chars: Vec<char> = line.chars().collect();
+            let number = &chars[1..];
+            let offset: i32 = number.iter().collect::<String>().parse()?;
+
+            if chars[0] == 'R' {
+                for _ in 0..offset {
+                    dial.rotate_right(1);
+                    if dial[50] == 0 {
+                        count += 1;
+                    }
+                }
+            } else {
+                for _ in 0..offset {
+                    dial.rotate_left(1);
+                    if dial[50] == 0 {
+                        count += 1;
+                    }
+                }
+            }
+        }
+
+        return Ok(count);
     }
 }
