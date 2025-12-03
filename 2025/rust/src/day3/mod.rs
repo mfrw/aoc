868b786fc3111c9c8f211c1f7f6230a54c709f47
Muhammad Falak R Wani <falakreyaz@gmail.com>
2025-12-03 11:37:38 +0530

    2025: rust: day3: impl part2
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- 2025/rust/src/day3/mod.rs
+++ 2025/rust/src/day3/mod.rs
@@ -3,30 +3,36 @@ use crate::utils;
 pub struct Solver;
 
 impl utils::Solver<3> for Solver {
-    type Part1 = u32;
+    type Part1 = usize;
 
     type Part2 = usize;
 
     fn part1(&self, input: &str) -> Result<Self::Part1, Box<dyn std::error::Error>> {
         let mut ans = 0;
         for line in input.lines() {
-            let mut mx = 0;
-            let c: Vec<u32> = line
-                .trim()
-                .chars()
-                .map(|c| c.to_digit(10).unwrap())
-                .collect();
-            for i in 0..c.len() {
-                for j in (i + 1)..c.len() {
-                    mx = mx.max(c[i] * 10 + c[j]);
-                }
-            }
-            ans += mx;
+            ans += max_batteries(line.as_bytes(), 2)
         }
         Ok(ans)
     }
 
     fn part2(&self, input: &str) -> Result<Self::Part2, Box<dyn std::error::Error>> {
-        todo!()
+        let mut ans = 0;
+        for line in input.lines() {
+            ans += max_batteries(line.as_bytes(), 12);
+        }
+        Ok(ans)
     }
+}
+
+fn max_batteries(xs: &[u8], l: usize) -> usize {
+    let mut r = String::new();
+    let mut j = 0;
+    for i in 0..l {
+        j = (j..xs.len() - l + i + 1)
+            .max_by_key(|&x| (xs[x], usize::MAX - x))
+            .unwrap();
+        r.push(xs[j] as char);
+        j += 1;
+    }
+    r.parse().unwrap()
 }
