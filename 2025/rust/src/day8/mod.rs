ae7b9a848343cc8af5de22ec75971545ed705100
Muhammad Falak R Wani <falakreyaz@gmail.com>
2025-12-08 12:57:16 +0530

    2025: rust: day8: impl part2
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- 2025/rust/src/day8/mod.rs
+++ 2025/rust/src/day8/mod.rs
@@ -5,7 +5,7 @@ pub struct Solver;
 impl utils::Solver<8> for Solver {
     type Part1 = usize;
 
-    type Part2 = usize;
+    type Part2 = i64;
 
     fn part1(&self, input: &str) -> Result<Self::Part1, Box<dyn std::error::Error>> {
         let boxes = input
@@ -52,7 +52,68 @@ impl utils::Solver<8> for Solver {
     }
 
     fn part2(&self, input: &str) -> Result<Self::Part2, Box<dyn std::error::Error>> {
-        todo!()
+        let boxes = input
+            .lines()
+            .map(|l| {
+                l.split(',')
+                    .map(|v| v.parse::<i64>().unwrap())
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
+
+        // calculate the distances between all pairs and sort them
+        let mut distances = Vec::new();
+        for a in 0..boxes.len() {
+            for b in a + 1..boxes.len() {
+                let dx = boxes[a][0].abs_diff(boxes[b][0]);
+                let dy = boxes[a][1].abs_diff(boxes[b][1]);
+                let dz = boxes[a][2].abs_diff(boxes[b][2]);
+                let d = dx * dx + dy * dy + dz * dz; // squared distance is enough
+                distances.push((d, a, b));
+            }
+        }
+        distances.sort_unstable_by_key(|d| d.0);
+
+        // create union-find data structure
+        let mut nodes = (0..boxes.len())
+            .map(|i| Node { parent: i, size: 1 })
+            .collect::<Vec<_>>();
+
+        // make the first 1000 connections and keep track of the cluster sizes
+        let mut sizes = vec![1; nodes.len()];
+        let mut n_clusters = nodes.len();
+        for &d in distances.iter().take(1000) {
+            let a = find(d.1, &mut nodes);
+            let b = find(d.2, &mut nodes);
+            if a != b {
+                n_clusters -= 1;
+                let parent = union(a, b, &mut nodes);
+                sizes[a] = 0;
+                sizes[b] = 0;
+                sizes[parent] = nodes[parent].size;
+            }
+        }
+
+        // take the product of the top 3 sizes
+        sizes.sort_unstable_by(|a, b| b.cmp(a));
+
+        // make the remaining connections
+        let mut ans = 0;
+        for &d in distances.iter().skip(1000) {
+            let a = find(d.1, &mut nodes);
+            let b = find(d.2, &mut nodes);
+            if a != b {
+                union(a, b, &mut nodes);
+                n_clusters -= 1;
+
+                if n_clusters == 1 {
+                    // there is only one cluster left
+                    ans = boxes[d.1][0] * boxes[d.2][0];
+                    break;
+                }
+            }
+        }
+        Ok(ans)
     }
 }
 
